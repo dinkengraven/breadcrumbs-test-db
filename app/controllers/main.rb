f9841b99719eff06b75a7d395e7b7954b902b11a
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-17 17:37:37 -0600

    add post route for new users

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -3,3 +3,14 @@ get "/users.json" do
   content_type :json
   return {:users => [users]}.to_json
 end
+
+post "/users" do
+  user = User.new(username: params[:username], email: params[:email])
+  if user.save
+    content_type :json
+    return {message: "success!"}
+  else
+    content_type :json
+    return {error: "user did not save"}
+  end
+end
