0b3866138b707ad622f0cbcd525b9be66e13a301
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-18 11:58:35 -0600

    update user model with bcrypt, assocs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   validates :username, { presence: true, uniqueness: true }
   validates :email, { presence: true, uniqueness: true }
 end
