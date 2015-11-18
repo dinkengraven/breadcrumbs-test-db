b3d6140d7a462289cc7b66a54db51a1fe24473f2
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-18 12:38:10 -0600

    add assocs for inverse_friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
 
   validates :first_name, { presence: true }
   validates :last_name, { presence: true }
